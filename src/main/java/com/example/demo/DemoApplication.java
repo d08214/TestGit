f9ef76e0c1ab9ebf8c33b6bbe77a862907951114
Andy <andy.xue@yimlink.com>
2021-02-25 16:11:30 +0800

    demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("1231");
         SpringApplication.run(DemoApplication.class, args);
     }
 
